89a794a7df5694fd56360144b4ff15c456d3add1
Rafael Nunes <angraralf@gmail.com>
2023-05-25 17:23:33 -0300

    :construction: object

---
+++ JavaScript - Stage 4/object.js
@@ -0,0 +1,17 @@
+/* 
+ Object 
+  * Objeto
+  * Propriedades / Atributos
+  * Funcionalidades / Métodos
+
+  { propridade: "valor" }
+
+*/ 
+
+console.log({
+  name: "Rafael",
+  idade: "34",
+  andar: function() {
+    console.log('andar')
+  }
+})
