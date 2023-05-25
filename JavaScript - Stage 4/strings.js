0fba455a4b7afc0dccb5597d9fbc6cdf2bb66c07
Rafael Nunes <angraralf@gmail.com>
2023-05-25 15:53:27 -0300

    :construction: strings

---
+++ JavaScript - Stage 4/strings.js
@@ -0,0 +1,12 @@
+console.log(`A soma de duas unidades é ${1+1}`)
+console.log("Isso é um texto") 
+
+/* 
+
+Cadeia de caracteres
+
+  "" aspas duplas 
+  '' aspas simples 
+  `` template literals ou template strings
+
+*/
