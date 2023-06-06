5aaee321644442458704e8ef9eb3dfa748417b8a
Rafael Nunes <angraralf@gmail.com>
2023-06-06 11:13:45 -0300

    :tada: stand 4

---
+++ Algoritmo e Logica com Javascript/notes.md
@@ -0,0 +1,6 @@
+Como resolver problemas na linguagem de programação ?
+
+Como melhorar minha lógica de programação ? 
+
+1. Identifique o que já sade e faça! 
+2. Quebre o grande problema em problemas menores!
---
+++ Algoritmo e Logica com Javascript/script.js
@@ -0,0 +1,24 @@
+// Comentário é feito assim
+
+/*
+Encontre a solução ao problema
+
+Pergunte o nome do usuário e escreva a mensagem: "Olá, [nome do usuário]"
+
+Como resolver problemas na linguagem de programação ?
+Como melhorar minha lógica de programação ? 
+
+
+
+*/
+
+/*
+  Encontre a solução ao problema:
+
+  Pergunte o nome do usuário e escreva a mensagem:
+  "Olá, [nome do usuário]"
+
+*/
+
+let nome = prompt("Qual o seu nome?")
+alert("Olá, " + nome)
