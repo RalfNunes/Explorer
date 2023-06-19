3301217399bc68dcd27eda8d692f6a5ed0e3358e
Rafael Nunes <angraralf@gmail.com>
2023-06-19 14:19:56 -0300

    ✨ finalizado o desafio

--- Algoritmo e Logica com Javascript/Desafio 1/script.js
+++ Algoritmo e Logica com Javascript/Desafio 1/script.js
@@ -7,23 +7,31 @@ numberTwo = [];
 let numberOne = prompt("Digite o primeiro número");
 let numberTwo = prompt("Digite o Segundo número");
 
+// Verificar se é igual
+function checkEqual() {
+  return numberOne === numberTwo ? 'são iguais' : 'são diferentes'
+}
+
 // Reatribuição
 numberOne = Number(numberOne);
 numberTwo = Number(numberTwo);
 
+
 // Calcúlos Matématica
 
 // Funcão Soma
-function sum() {
-  return numberOne + numberTwo;
-}
+let sum = (numberOne + numberTwo)
+
+// Par ou Ímpar 
+let parOuImpar = sum % 2 == 0 ? 'par' : 'ímpar'
+
 // Função Subtração
 function sub() {
-  return numberOne - numberTwo;
+  return (numberOne - numberTwo);
 }
 // Função Multiplicação
 function mult() {
-  return numberOne * numberTwo;
+  return (numberOne * numberTwo);
 }
 // Função Divisão
 function div() {
@@ -31,31 +39,18 @@ function div() {
 }
 // Função Resto
 function rest() {
-  return numberOne % numberTwo;
+  return (numberOne % numberTwo);
 }
 
-// Função de Igualdade
-function equal() {
-  if (numberOne == numberTwo) {
-    return alert("Números iguais")
-  } else return alert("Não são números iguais")
-}
+
 
 // Mensagem de alerta
 alert(`
-O resultado da soma ${sum(sum)}
+Os dois números inseridos ${checkEqual(checkEqual)}
+O resultado da soma ${sum}
+O resultado dos números é ${parOuImpar}
 O resultado da subtração ${sub(sub)}
 O resultado da multiplicação ${mult(mult)}
 O resultado da divisão ${div(div)}
 O resultado do Resto ${rest(rest)}
-O número é ${pImpar(pImpar)}
-Esses números são ${equal(equal)}
 `);
-
-// Função Par ou Impar
-function pImpar() {
-  if ((numberOne + numberTwo) % 2 == 0) {
-    return alert("Esse numero é Par");
-  } else return alert("Esse numero é Ímpar");
-}
-
