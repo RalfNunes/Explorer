1fe977727ea7a4e6dc27a1b00258a6c80005dc0f
Rafael Nunes <angraralf@gmail.com>
2023-07-07 17:30:29 -0300

    :heavy_check_mark: aplicando eventos e callbacks

--- Stage 05.0 - Jogo da adivinhação/main.js
+++ Stage 05.0 - Jogo da adivinhação/main.js
@@ -1,24 +1,46 @@
+// O ideal é criar constantes para o codigo que mais usa
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+
 // constante para criar um número randomico
 const randomNumber = Math.round(Math.random() * 10)
 // variavel de controle 
 let xAttempts = 1 
 
 // essa funcão feita para capturar o evento do "click"
-function handleClick(event) {
-  event.preventDefault();  // deixar o evento padrão sem carregar a pagina
+// função callback
+function handleTryClick(event) {
+  event.preventDefault();  // não faça o padrão do form
   const inputNumber = document.querySelector("#inputNumber");
   
 // se o número for igual o randomico
   if (Number(inputNumber.value) == randomNumber) {
     // achar a classe no html e adicionar 
-    document.querySelector(".screen1").classList.add("hide") 
+    screen1.classList.add("hide") 
     // achar a classe no html e remover
-    document.querySelector(".screen2").classList.remove("hide")
+    screen2.classList.remove("hide")
     // mostrar na proxima tela as tentativas 
     document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas`
   }
-  xAttempts++
 
-  console.log(randomNumber)
+  // deixar o input vazio
+  inputNumber.value =""
+
+
+  xAttempts++
 }
+
+// Eventos
+// criando uma constantes para os ids de botões
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
+// nome do evento 'click' depois colocar o função que vai reagir 
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', function() {
+  screen1.classList.remove("hide")
+  screen2.classList.add("hide")
+
+  xAttempts = 1
+})
 
