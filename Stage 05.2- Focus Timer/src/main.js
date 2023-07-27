2e07406fe1174c1428962bcefcfa55b41903ee2a
Rafael Nunes <angraralf@gmail.com>
2023-07-27 14:06:15 -0300

    :construction: construindo o modulo focustimer

--- Stage 05.2- Focus Timer/src/main.js
+++ Stage 05.2- Focus Timer/src/main.js
@@ -1,1 +1,8 @@
 import './toogle-dark.js'
+import './toogle-music.js'
+// o "*" significa tudo que esta sendo exportado, 
+// "as" é para renomear ou agrupar com o nome que quiser;
+// exportado "*" agrupar "as" em "nomeQualquer" from "arquivo"
+import * as FocusTimer from './focustimer/index.js'
+
+FocusTimer.start(0,5)
